f0b9a6c61c9e832c5ea5fbe6edb40000149feae4
manro01 <kenkut1@hotmail.com>
2025-02-28 14:43:12 -0600

    se creo una clase cuenta y su clase test, no se han probado

---
+++ src/main/java/ejemplos/models/Cuenta.java
@@ -0,0 +1,34 @@
+/**
+ * Representa una cuenta de banco simple
+ */
+package ejemplos.models;
+
+import java.math.BigDecimal;
+
+public class Cuenta
+{
+    private String persona;
+    private BigDecimal saldo;
+
+    public String getPersona()
+    {
+        return persona;
+    }
+
+    public void setPersona(String persona)
+    {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo()
+    {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo)
+    {
+        this.saldo = saldo;
+    }
+    
+    
+}
---
+++ src/test/java/ejemplos/models/CuentaTest.java
@@ -0,0 +1,15 @@
+/**
+ * Es la clase de prueba, en el caso del maestro, le dio las opciones para 
+ *  generar varios métodos, en este caso, no se si no supe como hacerlo pero
+ *  me genero muchos métodos por defautl, así que borre todo para hacerlo manual
+ *  como el maestro.
+ *  NOTA: La diferencia es que el maestro usa intelliJ
+ */
+package ejemplos.models;
+
+
+public class CuentaTest
+{
+    
+    
+}
